0624f1c39afd0085241ea1157025c13353f60fab
Evanski <61751485+EvanSkiStudios@users.noreply.github.com>
2024-11-22 12:32:11 -0500

    Updated to work with special keys like new lines and tab

--- AutoPaste/AutoPaste.py
+++ AutoPaste/AutoPaste.py
@@ -1,6 +1,7 @@
 import keyboard
 import win32clipboard
 from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
+# https://github.com/gauthsvenkat/pyKey/blob/master/pyKey/key_dict.py
 
 
 while True:  # making a loop
@@ -14,11 +15,17 @@ while True:  # making a loop
 
         # loop through string and press the keys
         for K in clipboardData:
+            print(repr(K))
+
             if K == "~":
                 K = "TILDE"
-            if K == " ":
+            elif K == " ":
                 K = "SPACEBAR"
-            print(K)
+            elif (K == "\r") or (K == "\n"):
+                K = "ENTER"
+            elif K == "\t":
+                K = "TAB"
+
             press(str(K))
         break
     else:
